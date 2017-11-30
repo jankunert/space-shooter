19d9eb301775d6417cf15fc2dc0e5b554741f015
Jan Marek Kunert <jan.kunert@r132-31-w10.SCHULE-ISERV>
2017-11-30 14:45:04 +0100

    1.7

---
+++ Assets/Scripts/BG.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG : MonoBehaviour {
+    public float scrollspeed;
+    public float tileSizedZ;
+    private Vector3 startPosition;
+	
+	void Start ()
+    {
+        startPosition = transform.position;
+       
+	}
+	
+	
+	void Update ()
+    {
+        float newPostion = Mathf.Repeat(Time.time * scrollspeed, tileSizedZ);
+        transform.position = startPosition + Vector3.forward * newPostion;
+    }
+		
+	
+}
